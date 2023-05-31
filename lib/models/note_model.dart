e25c3ee298cc624f14c8ba27262e89c48071b230
SaMi <32411864+Sami-Hegazy@users.noreply.github.com>
2023-05-31 13:38:37 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String note;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.note,
+    required this.date,
+    required this.color,
+  });
+}
